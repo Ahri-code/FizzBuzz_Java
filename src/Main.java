4004c41c0db2679fd78f50e029b6fdbd4b11da27
Mei <edygaldy302@gmail.com>
2024-04-22 21:03:11 +0200

    feat(java): added conditions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+// Read the number inputted by the user
+        Scanner read = new Scanner(System.in);
+        System.out.print("Please insert a number: ");
+        int number = read.nextInt();
+
+// Checks if the number is divisible for 5 and 3
+        if(number % 5 == 0 && number % 3 == 0) {
+            System.out.print("FizzBuzz");
+        }
+// If previous condition is false checks if the number is divisible for 3
+        else if(number % 3 == 0) {
+            System.out.print("Buzz");
+        }
+// If previous condition is false checks if the number is divisible for 5
+        else if(number % 5 == 0) {
+            System.out.print("Fizz");
+        }
+// If all the previous condition are false print the number inputted
+        else {
+            System.out.print(number);
+        }
     }
 }
