2fe10933ce208396fa6bd6a9ad651408fe816aba
Mei <edygaldy302@gmail.com>
2024-04-22 21:23:49 +0200

    feat(java): added do while loop

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,30 @@ public class Main {
     public static void main(String[] args) {
 // Read the number inputted by the user
         Scanner read = new Scanner(System.in);
-        System.out.print("Please insert a number: ");
-        int number = read.nextInt();
+        String exit;
+        do {
+            System.out.print("Please insert a number: ");
+            int number = read.nextInt();
 
 // Checks if the number is divisible for 5 and 3
-        if(number % 5 == 0 && number % 3 == 0) {
-            System.out.print("FizzBuzz");
-        }
+            if(number % 5 == 0 && number % 3 == 0) {
+                System.out.print("FizzBuzz\n");
+            }
 // If previous condition is false checks if the number is divisible for 3
-        else if(number % 3 == 0) {
-            System.out.print("Buzz");
-        }
+            else if(number % 3 == 0) {
+                System.out.print("Buzz\n");
+            }
 // If previous condition is false checks if the number is divisible for 5
-        else if(number % 5 == 0) {
-            System.out.print("Fizz");
-        }
+            else if(number % 5 == 0) {
+                System.out.print("Fizz\n");
+            }
 // If all the previous condition are false print the number inputted
-        else {
-            System.out.print(number);
-        }
+            else {
+                System.out.print(number + "\n");
+            }
+// Checks if the user wants to stop
+            System.out.print("Would you like to stop? ");
+            exit = read.next().toLowerCase();
+        } while(!exit.equals("yes"));
     }
 }
